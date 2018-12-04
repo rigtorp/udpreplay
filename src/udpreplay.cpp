252fa87fa58c4f9f89cb4b325ffa083d820ae050
Erik Rigtorp <erik@rigtorp.se>
2018-12-04 15:13:58 -0600

    Use existing macro for VLAN ethertype

--- src/udpreplay.cpp
+++ src/udpreplay.cpp
@@ -140,7 +140,6 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  const std::uint16_t ETHERTYPE_802_1Q = 0x8100;
   pcap_pkthdr header;
   const u_char *p;
   timeval tv = {0, 0};
@@ -151,7 +150,7 @@ int main(int argc, char *argv[]) {
     auto eth = reinterpret_cast<const ether_header *>(p);
     
     // jump over and ignore vlan tag 
-    if (ntohs(eth->ether_type) == ETHERTYPE_802_1Q) {
+    if (ntohs(eth->ether_type) == ETHERTYPE_VLAN) {
        p += 4;
        eth = reinterpret_cast<const ether_header *>(p);
     }
