8765feca3ddbdc71a02bb9bb150618ae6ca01447
Maksym Sobolyev <sobomax@sippysoft.com>
2019-12-04 15:52:11 -0800

    Add some basic functionality testing around new timing code.

---
+++ tests/basic.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+_TCMD="/usr/bin/time"
+
+linux_time()
+{
+  "${_TCMD}" -f "\t%e real\t%U user\t%s sys" "${@}"
+}
+
+if "${_TCMD}" -f "" echo
+then
+  TCMD="linux_time"
+else
+  TCMD="${_TCMD}"
+fi
+  
+TCASE="basic_lowres"
+TFNAME="${TCASE}.timings"
+RESFILE="${TFNAME}"
+CORRFILE="../tests/${TFNAME}"
+PFILE="../tests/${TCASE}.pcap"
+
+rm -f "${RESFILE}"
+for speed in 0.1 0.5 1.0 1.5 1.9
+do
+  "${TCMD}" ./udpreplay -s ${speed} "${PFILE}" 2>&1 | \
+   awk '{print $1}' | sed 's|[0-9]$||' >> "${RESFILE}"
+done
+diff -u "${CORRFILE}" "${RESFILE}"
+echo "Looks good!"
