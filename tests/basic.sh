03ad2b1b1146d1eb0d08fc6f625c6f9348e09da0
Maksym Sobolyev <sobomax@sippysoft.com>
2019-12-04 16:00:29 -0800

    Produce some progress output while running.

--- tests/basic.sh
+++ tests/basic.sh
@@ -9,7 +9,7 @@ linux_time()
   "${_TCMD}" -f "\t%e real\t%U user\t%s sys" "${@}"
 }
 
-if "${_TCMD}" -f "" echo
+if "${_TCMD}" -f "" echo 2>/dev/null
 then
   TCMD="linux_time"
 else
@@ -25,6 +25,7 @@ PFILE="../tests/${TCASE}.pcap"
 rm -f "${RESFILE}"
 for speed in 0.1 0.5 1.0 1.5 1.9
 do
+  echo "- replaying ${TCASE} at (speed / ${speed})x..."
   "${TCMD}" ./udpreplay -s ${speed} "${PFILE}" 2>&1 | \
    awk '{print $1}' | sed 's|[0-9]$||' >> "${RESFILE}"
 done
